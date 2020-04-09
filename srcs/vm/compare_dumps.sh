1eacb58d48c6c0d51c40063b72c35162eff79a07
dkotenko <dmitrii.kotenko@gmail.com>
2020-04-09 20:45:22 +0300

    compare.sh

--- srcs/vm/compare_dumps.sh
+++ srcs/vm/compare_dumps.sh
@@ -1,2 +1,2 @@
-make && ./corewar jumper.cor jumper.cor -d $1 > team.txt && 
-../../../corewar-eleonard/corewar -d $1 jumper.cor jumper.cor > elen.txt
+make && ./corewar jumper.cor jumper.cor -d $1 > 1177.txt && 
+../../../corewar-eleonard/corewar -d $1 jumper.cor jumper.cor > origin.txt
